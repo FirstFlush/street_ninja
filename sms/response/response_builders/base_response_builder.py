643f940d85832c0f0d49e229aa112f6987a81956
FirstFlush <firstflush@protonmail.com>
2025-02-12 12:50:01 -0800

    removed ResponseWrapper class

--- sms/response/response_builders/base_response_builder.py
+++ sms/response/response_builders/base_response_builder.py
@@ -1,8 +1,5 @@
 from abc import ABC
 import logging
-from twilio.twiml.messaging_response import MessagingResponse
-from sms.enums import SMSKeywordEnum
-from ..response_templates import BaseSMSResponseTemplate
 
 
 logger = logging.getLogger(__name__)
@@ -12,13 +9,3 @@ class BaseResponseBuilder(ABC):
 
     def __init__(self):
         ...
-
-
-    # def to_twiml(self, msg: str) -> str:
-    #     """
-    #     Converts the response text to TwiML, a type of XML that Twilio Gateway requires.
-    #     """
-    #     mr = MessagingResponse()
-    #     mr.message(msg)
-    #     logger.info("Created TwiML response")
-    #     return str(mr)
