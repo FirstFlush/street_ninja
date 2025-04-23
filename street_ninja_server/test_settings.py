97bf3375f4b32d20b4c89fd61605fd00cd77aa21
FirstFlush <firstflush@protonmail.com>
2025-04-22 18:21:05 -0700

    added templates config to CI pipeline

--- street_ninja_server/test_settings.py
+++ street_ninja_server/test_settings.py
@@ -113,6 +113,22 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'street_ninja_server.urls'
 WSGI_APPLICATION = 'street_ninja_server.wsgi.application'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 DATABASES = {
     "default": {
         "ENGINE": "django.contrib.gis.db.backends.postgis",
