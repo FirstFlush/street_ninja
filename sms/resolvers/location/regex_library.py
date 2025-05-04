5dd475fe2b9f3b5ecc674f36b0006ffa14d27116
FirstFlush <firstflush@protonmail.com>
2025-05-03 20:35:45 -0700

    removed commented-out patterns

--- sms/resolvers/location/regex_library.py
+++ sms/resolvers/location/regex_library.py
@@ -7,11 +7,8 @@ class RegexLibrary:
     street_suffix_after_word = rf"\b({StreetSuffixEnum.regex_string()})\b"
     street_direction_after_suffix = rf"\b({StreetDirectionEnum.regex_string()})\b"
     intersection = r"\b(\w+)\s+(?:&|and)\s+(\w+)\b"
-    # normalize_string = re.compile(r"[^\w\s]")
     normalize_string = re.compile(r"[^\w\s&]")  # Strip special chars but keep `&` since it appears intersections
     multiple_whitespace = re.compile(r"\s+")    # collapse multiple whitespace:  "   " -> ""
-    # full_address = rf"\b(\d+[-#]?\w*)\s+(\w+(\s+\w+)*)\s+({StreetSuffixEnum.regex_string()})(\s+({StreetDirectionEnum.regex_string()}))?\b"
-
     full_address = fr"""
 \b
 (?:[#A-Z]?-?\d+[-]?\d*)\s+
