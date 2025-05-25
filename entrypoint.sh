573583a647098e15554f26651a0a7e93ef41e3c5
FirstFlush <firstflush@protonmail.com>
2025-05-25 14:41:27 -0700

    moved startup logic from CI docker-compose test-runner command declaration to entrypoint.sh script. uncommented test in resources/tests/test_map_routes.py

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,21 +9,17 @@
 # This script is executed as the Dockerfile's ENTRYPOINT.
 set -e
 
-if [ "$CI" != "True" ]; then
-    echo "Waiting for Postgres..."
-    until pg_isready -h db -p 5432; do
-    sleep 1
-    done
-    sleep 2
+echo "Waiting for Postgres..."
+until pg_isready -h db -p 5432; do
+sleep 1
+done
+sleep 2
 
-    echo "Applying database migrations..."
-    python manage.py migrate
+echo "Applying database migrations..."
+python manage.py migrate
 
-    echo "Running get_locations..."
-    python manage.py get_neighborhoods
-else
-    echo "CI environment detected, skipping entrypoint.sh setup logic..."
-fi
+echo "Running get_locations..."
+python manage.py get_neighborhoods
 
 echo "Starting server..."
 exec "$@"
