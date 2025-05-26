ba3d9eebb8280191fe972a8b1380faf952758322
FirstFlush <firstflush@protonmail.com>
2025-05-25 17:05:52 -0700

    created new docker container 'bootstrap' to handle DB migrations and other management commands. changed entrypoint.sh role to confirming DB connection

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 #
-# Entrypoint script for the Django container.
-# - Waits for Postgres
-# - Applies database migrations
-# - Loads neighborhoods
-# - Starts Gunicorn
+# Wait for DB to be accepting requests before moving further.
 #
 # This script is executed as the Dockerfile's ENTRYPOINT.
 set -e
@@ -15,11 +11,4 @@ sleep 1
 done
 sleep 2
 
-echo "Applying database migrations..."
-python manage.py migrate
-
-echo "Running get_locations..."
-python manage.py get_neighborhoods
-
-echo "Starting server..."
 exec "$@"
