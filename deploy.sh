32e4723605b067092ede5504d24c59c8f4f9676b
FirstFlush <firstflush@protonmail.com>
2025-05-25 17:51:04 -0700

    fix deploy script to run bootstrap container

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,12 @@
 # - Cleans up old images/containers
 # - Starts the app
 
-set -e  # Exit on error
+set -e
+
+if [ "$EUID" -eq 0 ]; then
+  echo "Do not run this script with sudo. Try it again as $USER."
+  exit 1
+fi
 
 ENV_FILE=".env.prod"
 
@@ -29,29 +34,34 @@ sudo -v  # trigger sudo auth upfront
 echo "Starting deployment..."
 
 if [ "$SKIP_PULL" = false ]; then
-  echo "[1/6] Pulling latest changes from Git..."
+  echo "[1/7] Pulling latest changes from Git..."
   git pull origin master
 else
-  echo "[1/6] Skipping git pull (per --no-pull)"
+  echo "[1/7] Skipping git pull (per --no-pull)"
 fi
 
-echo "[2/6] Stopping existing containers..."
+echo "[2/7] Stopping existing containers..."
 docker compose down
 
-echo "[3/6] Removing old containers and pruning images..."
+echo "[3/7] Removing old containers and pruning images..."
 docker compose rm -f
 docker container prune -f
 docker image prune -af
 docker network prune -f
 
-echo "[4/6] Rebuilding containers with .env.prod..."
+echo "[4/7] Rebuilding containers with .env.prod..."
 docker compose --env-file $ENV_FILE build --no-cache
 
-echo "[5/6] Starting containers..."
+echo "[5/7] Running bootstrap tasks..."
+docker compose --env-file $ENV_FILE run --rm bootstrap
+
+echo "[6/7] Starting containers..."
 docker compose --env-file $ENV_FILE up -d
 
-echo "[6/6] Cleaning up build cache..."
+echo "[7/7] Cleaning up build cache..."
 docker builder prune --all --force
 
+echo
 echo "[SUCCESS] Deployment complete."
+echo
 echo "Reload Nginx server by running the command 'sudo systemctl reload nginx'"
